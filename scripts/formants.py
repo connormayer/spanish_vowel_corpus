ba9ecdd334c64f3c2dc9451e486c1943b38c0976
mlwe <brightoutlier@gmail.com>
2024-03-12 16:05:05 -0700

    Added Missing Files scripts

--- scripts/formants.py
+++ scripts/formants.py
@@ -5,13 +5,17 @@ from fasttrackpy import process_audio_file, \
     process_directory, \
     process_audio_textgrid,\
     process_corpus
+import parselmouth as pm
+
+import numpy as np
+import pandas as pd
 
 import matplotlib.pyplot as plt
 import math as m
 
 
 # Set directories
-in_path = "/Users/meganlwe/Documents/GitHub/spanish_vowel_corpus/audio/word_segmented/"
+in_path = "/Users/meganlwe/Documents/GitHub/spanish_vowel_corpus/audio/word_segmented"
 os.chdir(in_path)
 
 out_path = "/Users/meganlwe/Documents/GitHub/spanish_vowel_corpus/spectrograms/"
@@ -22,25 +26,76 @@ if os.path.isdir(out_path):
 os.mkdir(out_path)
 
 
-# word_segmented file names
-files = os.listdir()
-files_list = list(set([file.split(".")[0] for file in files if not ""]))
-#temp_list = files_list[:5]
+
+results = process_corpus(corpus_path = in_path,
+                         entry_classes=['words', 'vowels'],
+                         target_tier='vowels', target_labels='[aeiou]',
+                         min_duration=0.02,
+                         min_max_formant=3000, max_max_formant=7000,
+                         nstep=20, n_formants=5,
+                         time_step=0.002)
+print("Processed Corpus")
+
+track_dicts = []
+for track in results:
+    # Save winner dataframe
+    df = track.to_df(which = "winner").to_pandas()
+    rows = len(df)
+    midpoint = rows // 2
+
+    file = df["file_name"][0]
+    #sound = pm.Sound(os.path.join(in_path,file+".wav"))
+
+    curr_dict = {}
+    curr_dict.update({
+        "Filename": file,
+        "Subject": df["file_name"][0].split("_")[0],
+        "Gender": df["file_name"][0].split("_")[1],
+        "Vowel": df["label"][0],
+        "Word": df["file_name"][0].split("_")[2],
+        "F0": np.mean(track.sound.to_pitch().selected_array["frequency"])
+        #np.mean(track.sound.to_pitch(pitch_ceiling = 300).selected_array["frequency"])
+        })
+    #to_pitch_ac()
+    #pitch ceiling for m=200/f=250
+
+    for i in range(1, 4):
+        fx_df = df["F{}".format(i)]
+        curr_dict.update({
+            "F{}_{}".format(i, x):
+            fx_df.iloc[np.percentile(range(rows),
+                                     x, method = "nearest")]
+            for x in range(10,100,10)
+            })
+    curr_dict.update({
+        "F4": df["F4"].iloc[midpoint],
+        "Duration": track.sound.duration
+        })
+    track_dicts.append(curr_dict)
+
+    # Spectrogram Plot
+    track.spectrograms()
+    curr_plt = plt.gcf()
+    curr_plt.suptitle(file, y=0.95, fontsize=16)
+    curr_plt.supxlabel("Time (s)", y=0.04)
+    curr_plt.supylabel("Frequency (Hz)", x = 0.05)
+    curr_plt.savefig(out_path+file+'.png')
+    plt.close()
+    print(file+" finished")
 
 
-temp_list = ["subj41_f_cuco_98"]
-for file in temp_list:
-    #print(file)
-    audio_name = file+".wav"
-    tg_name = file+".TextGrid"
-    results = process_audio_textgrid(audio_path = os.path.join(in_path, audio_name),
-                           textgrid_path = os.path.join(in_path, tg_name),
-                           entry_classes=['words', 'vowels'],
-                           target_tier='vowels', target_labels='[aeiou]',
-                           min_duration=0.02, min_max_formant=3000,
-                           max_max_formant=7000, nstep=20,
-                           n_formants=5)
-    breakpoint()
+data = pd.DataFrame(track_dicts)
+breakpoint()
+
+
+                                                                                     
+
+
+
+
+
+'''
+
     
     #file_df = results[0].to_df(which = "all").to_pandas()
     #file_df = file_df[["F1", "F2", "F3", "F4", "max_formant", "time"]]
@@ -49,37 +104,11 @@ for file in temp_list:
     #for i in range(10):
         #pd.DataFrame({'':[]})
 
+subj14_m_    _saque_0.TextGrid
+subj14_m_    _saque_0.wav
 
-    if results:
-        results[0].spectrograms()
-        curr_plt = plt.gcf()
-        curr_plt.suptitle(file, y=0.95, fontsize=16)
-        curr_plt.supxlabel("Time (s)", y=0.04)
-        curr_plt.supylabel("Frequency (Hz)", x = 0.05)
-        curr_plt.savefig(out_path+file+'.png')
-        plt.close()
-
-        results[0].winner.spectrogram()
-        curr_win = plt.gcf()
-        curr_win.suptitle(file+"_winner", y=0.95, fontsize=16)
-        curr_win.savefig(out_path+file+'_win.png')
-        plt.close()
-
-    else:
-        print("results for {} is empty".format(file))
+subj19_f_quite_  _saca_261.TextGrid
+subj19_f_quite_  _saca_261.wav
 
 
-
-
-'''
-
-
-process_corpus(corpus_path = path, entry_classes=['Words', 'Vowels'],
-               target_tier='Words', target_labels='[AEIOU]',
-               min_duration=0.05,
-               min_max_formant=4000, max_max_formant=7000,
-               nstep=20, n_formants=4,
-               window_length=0.025, time_step=0.002,
-               pre_emphasis_from=50, smoother=Smoother(),
-               loss_fun=Loss(), agg_fun=Agg())
 '''
---
+++ scripts/segment_files_check.py
@@ -0,0 +1,45 @@
+import os
+import re
+
+import pandas as pd
+
+def create_string_name(subject, word):
+    regex_pattern = fr'{subject}_\w_{re.escape(word)}'
+    return re.compile(regex_pattern)
+
+path = "/Users/meganlwe/Documents/GitHub/spanish_vowel_corpus/"
+words_list = pd.read_csv(os.path.join(path, "audio", "subject_orderings",
+                                      "subject-1.csv"))["word"].unique()
+words_list = [word.lower() for word in words_list]
+
+audio_files = os.listdir(os.path.join(path, "audio", "word_segmented"))
+
+missing_subj = []
+for subj in range(1, 82):
+    subj_files = [file for file in audio_files
+                  if file.startswith("subj{}_".format(subj))]
+    if len(subj_files) != 160:
+        missing_subj += ["subj{}".format(subj)]
+
+missing_words = []
+
+for subj in missing_subj:
+    for word in words_list:
+        string = create_string_name(subj, word)
+        match_list = [file for file in audio_files if string.match(file)]
+
+        found = len(match_list)
+        if found<4:
+            curr_dict = {
+                "Subject": subj,
+                "Word": word,
+                "Count": found}
+            missing_words.append(curr_dict)
+
+pd.DataFrame(missing_words).to_csv(os.path.join(path, "data",
+                                                "missing_TG.csv"),
+                                   index = False)
+
+print("done")
+
+
