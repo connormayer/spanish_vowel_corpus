51841b2f6c9fee6fa638baae840c0ca3fec51d97
mlwe <brightoutlier@gmail.com>
2024-07-01 15:14:50 -0700

    Update vowel_distribution.R

--- scripts/vowel_distribution.R
+++ scripts/vowel_distribution.R
@@ -5,13 +5,13 @@ path <- "/Users/meganlwe/Documents/GitHub/spanish_vowel_corpus"
 # Importing Data 
 setwd(file.path(path, "data"))
 df <- read_csv("ft_results.csv") %>%
-  select(Gender, Vowel, ends_with("_50")) %>%
+  select(Gender, Vowel, ends_with("_50"), F4) %>%
   group_by(Gender, Vowel)
 
 #Compute Means and Standard Deviations
 summ_stats <- df %>% 
   summarize(across(starts_with("F"), 
-                   list(mean = mean,
+                   list(mean = mean, #drop.na
                         sd = sd),
                    .names = "{col}_{fn}"))
 
@@ -31,7 +31,7 @@ f_prob <- function(value, gender, vowel, formant){
   return(dnorm(value, mu, sigma)*100) 
 }
 
-f_prob(80, "f", "a", "F1") # example
+f_prob(800, "m", "a", "F1") # example
 
 # Histograms
 mf_df <- read_csv("max_formants.csv")
@@ -40,12 +40,15 @@ subj_list = subj_list[1]
 for (subj in subj_list){
   subj_df <- mf_df %>%
     filter(Subject == subj) %>%
+    filter(Vowel == "a") %>%
     select(-Subject)
   
-  vowel_df <- subj_df %>%
-    ggplot(aes(x="Max Formant", fill = "Vowel")) +
-    geom_histogram(color = "black", bins = 20, alpha = 0.7) +
+  subj_df %>%
+    ggplot(aes(x="Max Formant")) +
+    geom_histogram(color = "black", bins = 20, alpha = 0.7, stat="count") +
     #labs(title = ) +
     theme_minimal()
 }
+
+mf_df %>% group_by(Subject) %>% summarize(median(`Max Formant`))
 
